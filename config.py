d6389d5a23e765c146d30f55821d3e91bd82bef9
YuraDnistriansky <yura.dniester@gmail.com>
2025-09-18 23:28:39 +0300

    fix config

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import yaml
+from my import jwt_secret_key, db_password, db_host
 
 with open("config/config.yml", "r") as file:
     config = yaml.safe_load(file)
@@ -7,11 +8,11 @@ class Config:
     JWT_TOKEN_LOCATION = config["jwt"]["token_location"]
     JWT_HEADER_NAME = config["jwt"]["header_name"]
     JWT_HEADER_TYPE = config["jwt"]["header_type"]
-    JWT_SECRET_KEY = config["jwt"]["secret_key"]
+    JWT_SECRET_KEY = jwt_secret_key
 
     DB_USER = config["database"]["user"]
-    DB_PASSWORD = config["database"]["password"]
-    DB_HOST = config["database"]["host"]
+    DB_PASSWORD = db_password
+    DB_HOST = db_host
     DB_PORT = config["database"]["port"]
     DB_NAME = config["database"]["name"]
     SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
