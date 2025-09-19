8dcd8b32428cbefeb61b57142f83f36b1918f79a
YuraDnistriansky <yura.dniester@gmail.com>
2025-09-19 20:32:12 +0300

    delete config.yml

--- config.py
+++ config.py
@@ -1,18 +1,15 @@
-import yaml
 from my import jwt_secret_key, db_password, db_host
 
-with open("config/config.yml", "r") as file:
-    config = yaml.safe_load(file)
-
 class Config:
-    JWT_TOKEN_LOCATION = config["jwt"]["token_location"]
-    JWT_HEADER_NAME = config["jwt"]["header_name"]
-    JWT_HEADER_TYPE = config["jwt"]["header_type"]
+    JWT_TOKEN_LOCATION = ["headers"]
+    JWT_HEADER_NAME = "Authorization"
+    JWT_HEADER_TYPE = "Bearer"
     JWT_SECRET_KEY = jwt_secret_key
 
-    DB_USER = config["database"]["user"]
+    DB_USER = "admin"
     DB_PASSWORD = db_password
     DB_HOST = db_host
-    DB_PORT = config["database"]["port"]
-    DB_NAME = config["database"]["name"]
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    DB_PORT = 3306
+    DB_NAME = "database-1"
+    # SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    SQLALCHEMY_DATABASE_URI = "mysql://root:RTXSQl__23!sQl@127.0.0.1:3306/labWork5_2"
