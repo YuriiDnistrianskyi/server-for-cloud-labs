177abf78a2ee6dff7fd565e2eef8805601a0788d
YuraDnistriansky <yura.dniester@gmail.com>
2025-09-26 23:33:03 +0300

    create dockerfile

--- config.py
+++ config.py
@@ -1,14 +1,15 @@
-from my import jwt_secret_key, db_password, db_host
+import os
+from my import db_password
 
 class Config:
     JWT_TOKEN_LOCATION = ["headers"]
     JWT_HEADER_NAME = "Authorization"
     JWT_HEADER_TYPE = "Bearer"
-    JWT_SECRET_KEY = jwt_secret_key
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
 
-    DB_USER = "admin"
-    DB_PASSWORD = db_password
-    DB_HOST = db_host
+    DB_USER = os.getenv("DB_USER", "root")
+    DB_PASSWORD = os.getenv("DB_PASSWORD", db_password)
+    DB_HOST = os.getenv("DB_HOST")
     DB_PORT = 3306
     DB_NAME = "database-1"
     SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
