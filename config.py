2d7c0c2aba2efa3da2805ca7da72c0f13bd78f4d
YuraDnistriansky <yura.dniester@gmail.com>
2025-10-03 01:57:44 +0300

    mini fix

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 import os
-from my import db_password
+from my import jwt_secret_key, db_password, db_host
 
 class Config:
     JWT_TOKEN_LOCATION = ["headers"]
     JWT_HEADER_NAME = "Authorization"
     JWT_HEADER_TYPE = "Bearer"
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", jwt_secret_key)
 
-    DB_USER = os.getenv("DB_USER", "root")
+    DB_USER = os.getenv("DB_USER", "admin")
     DB_PASSWORD = os.getenv("DB_PASSWORD", db_password)
-    DB_HOST = os.getenv("DB_HOST")
+    DB_HOST = os.getenv("DB_HOST", db_host)
     DB_PORT = 3306
     DB_NAME = "database-1"
     SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
